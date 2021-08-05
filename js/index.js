8f401e23c8cd0f1f613ea95eefb7b06a015305c4
Franklin Huichi <carloshuichi@gmail.com>
2021-08-05 17:01:46 -0500

    terminando skills

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
-function openSkill(evt, cityName) {
+
+function openSkill(evt, skillName) {
     let i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName("item1__container--skill");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName("item1__container--tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(skillName).style.display = "block";
     evt.currentTarget.className += " active";
   }
