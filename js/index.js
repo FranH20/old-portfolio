84658eb2e7038ff2e210b6f309b86687e9207f15
Franklin Huichi <carloshuichi@gmail.com>
2021-08-06 22:32:20 -0500

    primera version

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
-
+function changeFunc() {
+  var selectBox = document.getElementById("selectBox");
+  var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+  openSkill(Event, selectedValue)
+}
 function openSkill(evt, skillName) {
     let i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("item1__container--skill");
+    tabcontent = document.getElementsByClassName("item__container--skill");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("item1__container--tablinks");
+    tablinks = document.getElementsByClassName("item__container--tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     document.getElementById(skillName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+/* 
+$('.items__left').click(function(){
+  $(this).next().slideToggle();
+})*/
