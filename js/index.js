bd462bd70047fbf3b1fa3d4b505b6dc356ef188b
Franklin Huichi <carloshuichi@gmail.com>
2021-08-08 01:13:31 -0500

    avanzando experiencia

--- js/index.js
+++ js/index.js
@@ -4,19 +4,28 @@ function changeFunc() {
   openSkill(Event, selectedValue)
 }
 function openSkill(evt, skillName) {
-    let i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("item__container--skill");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("item__container--tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(skillName).style.display = "block";
-    evt.currentTarget.className += " active";
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("item__container--skill");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-/* 
-$('.items__left').click(function(){
-  $(this).next().slideToggle();
-})*/
+  tablinks = document.getElementsByClassName("item__container--tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(skillName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openAccordion(blockName){
+  block_element = document.getElementById(blockName);
+  
+  if (block_element.style.display === 'none'){
+    console.log('arr1')
+    block_element.style.display = 'flex';
+  }else{
+    console.log('arr2')
+    block_element.style.display = 'none';
+  }
+}
+
